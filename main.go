e7328d80c11536392f31a66963e61bce483ec5db
hattiza <guardian.flame0631@fastmail.com>
2023-11-26 22:34:51 +0000

    Have a looping conversation with Eliza

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"main/doctor"
+	"os"
 )
 
 func main() {
-	var whatToSay string
-	whatToSay = doctor.Intro()
+	reader := bufio.NewReader(os.Stdin)
+
+	whatToSay := doctor.Intro()
 	saySomething(whatToSay)
+
+	for {
+		fmt.Print("-> ")
+		userInput, _ := reader.ReadString('\n')
+		response := doctor.Response(userInput)
+		fmt.Println(response)
+
+	}
 }
 
 func saySomething(something string) {
